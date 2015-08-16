cba06256ec2a9249e75a0c2d96f80a4de1194392
Diane Van Etten <diane.vanetten@me.com>
2015-08-16 23:51:14 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shad0w::Application.config.secret_key_base = '8aa9035179decb8d92bfd8257b9a2584e8baf47454918f42de2685edbb83376874a66459b7cb3c5c3599d7476e7c82e1fdac26d20ca4171b26a51ff6fe0be9ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shad0w::Application.config.session_store :cookie_store, key: '_shad0w_session'
